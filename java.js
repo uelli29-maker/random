4a3517e4a67a773275b8bb4ac93765ed5ac75987
uelli29-maker <uelli29@gmail.com>
2025-10-15 22:36:15 -0300

    randon

---
+++ java.js
@@ -0,0 +1,17 @@
+function generateNumber(){
+
+const min = document.querySelector(".de_0").value
+const max = document.querySelector(".a-100").value
+
+
+const result = Math.floor(Math.random() * (max - min + 1)) + parseInt(min)
+
+document.querySelector(".result").innerHTML = alert (result) 
+
+
+
+
+
+}
+
+
